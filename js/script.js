76299da862c0dbc2f5767e21762f20e974b759b8
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2020-11-06 12:16:39 +0100

    Add code for player move

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ else if(randomNumber==3){
     computerMove="nożyce";
 }
 printMessage("Mój ruch to: "+computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+    playerMove = 'papier';
+}
+else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
